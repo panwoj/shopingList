0aa31de0b18b47bfa8ca9a6e0923299200c23c9d
panwoj <pan_woj@poczta.onet.pl>
2018-06-04 22:30:18 +0200

    Add DbService

---
+++ src/main/java/com/shoppinglist/service/DbService.java
@@ -0,0 +1,22 @@
+package com.shoppinglist.service;
+
+import com.shoppinglist.domain.Item;
+import com.shoppinglist.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DbService {
+    @Autowired
+    private ItemRepository itemRepository;
+
+    public Item saveItem(final Item item) {
+        return itemRepository.save(item);
+    }
+
+    public List<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+}
