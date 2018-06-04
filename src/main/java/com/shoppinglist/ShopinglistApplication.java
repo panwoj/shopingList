5fe07e4ca04d30fc495d4885ea47d3d5415564ff
panwoj <pan_woj@poczta.onet.pl>
2018-06-04 21:49:23 +0200

    change ShopinglistApplication.java

--- src/main/java/com/shoppinglist/ShopinglistApplication.java
+++ src/main/java/com/shoppinglist/ShopinglistApplication.java
@@ -1,4 +1,4 @@
-package com.shoppinglist.shopinglist;
+package com.shoppinglist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
