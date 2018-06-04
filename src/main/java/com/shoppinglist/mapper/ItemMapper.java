c6d1131f558c2de52a09d032b44090a77f713516
panwoj <pan_woj@poczta.onet.pl>
2018-06-04 22:28:52 +0200

    Change ItemMapper

--- src/main/java/com/shoppinglist/mapper/ItemMapper.java
+++ src/main/java/com/shoppinglist/mapper/ItemMapper.java
@@ -4,13 +4,18 @@ import com.shoppinglist.domain.Item;
 import com.shoppinglist.domain.ItemDto;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class ItemMapper {
     public Item mapToItem(final ItemDto itemDto) {
         return new Item(itemDto.getId(), itemDto.getName(), itemDto.getAmount());
     }
 
-    public ItemDto mapToItemDto(final Item item) {
-        return new ItemDto(item.getId(), item.getName(), item.getAmount());
+    public List<ItemDto> mapToItemDtoList(final List<Item> itemList) {
+        return itemList.stream()
+                .map(i -> new ItemDto(i.getId(), i.getName(), i.getAmount()))
+                .collect(Collectors.toList());
     }
 }
