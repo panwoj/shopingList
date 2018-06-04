db1747ec8d41051ac10ea341da0c335be73ae505
panwoj <pan_woj@poczta.onet.pl>
2018-06-04 22:29:26 +0200

    Add ItemController

---
+++ src/main/java/com/shoppinglist/controller/ItemController.java
@@ -0,0 +1,34 @@
+package com.shoppinglist.controller;
+
+import com.shoppinglist.domain.Item;
+import com.shoppinglist.domain.ItemDto;
+import com.shoppinglist.mapper.ItemMapper;
+import com.shoppinglist.service.DbService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping("/v1")
+@CrossOrigin(origins = "*")
+@AllArgsConstructor
+public class ItemController {
+    @Autowired
+    private DbService dbService;
+    private ItemMapper itemMapper;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/items")
+    public List<ItemDto> getItems() {
+        return itemMapper.mapToItemDtoList(dbService.getAllItems());
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/items", consumes = APPLICATION_JSON_VALUE)
+    public Item addItem(@RequestBody ItemDto itemDto) {
+        return dbService.saveItem(itemMapper.mapToItem(itemDto));
+    }
+
+}
